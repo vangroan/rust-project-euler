a30b80daf70154c9830f089937ebae6ffc649cae
Willem Victor <wimpievictor@gmail.com>
2017-05-18 19:31:43 +0200

    Began with problem 5

--- src/main.rs
+++ src/main.rs
@@ -86,4 +86,15 @@ fn problem_004() -> i64{
 }
 
 
+/// 2520 is the smallest number that can be divided by each of the numbers from 
+/// 1 to 10 without any remainder.
+///
+/// What is the smallest positive number that is evenly divisible by all of the 
+/// numbers from 1 to 20?
+#[allow(dead_code)]
+fn problem_005() -> i64 {
+    0
+}
+
+
 fn main() {}
--- src/tests.rs
+++ src/tests.rs
@@ -18,7 +18,14 @@ fn test_problem_003() {
     assert_eq!(problem_003(), 6857);
 }
 
+
 #[test]
 fn test_problem_004() {
     assert_eq!(problem_004(), 906609);
+}
+
+
+#[test]
+fn test_problem_005() {
+    assert_eq!(problem_005(), -1);
 }
